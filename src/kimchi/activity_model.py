22ea1a6dad568a8f7117fa1d5986c07d3c4c8349
filip <f.trojan@centrum.cz>
2024-10-05 09:49:34 +0200

    refactor update_score

--- src/kimchi/activity_model.py
+++ src/kimchi/activity_model.py
@@ -13,32 +13,20 @@ def get_scores(obs_data: pd.DataFrame) -> pd.DataFrame:
 
 
 def pd_activity_score(df: pd.DataFrame) -> pd.Series:
-    days_history = (df.tst.max() - df.tst.min()).days
     score = 0.0
-    tst_last_session: pd.Timestamp | None = None
-    tst_last_event: pd.Timestamp | None = None
-    days_since_last_session: float | None = None
-    days_since_last_event: float | None = None
     for x in df.itertuples():
-        score = update_score(score, days_since_last_event, days_since_last_session, x.event_type, x.se_action)
-        if x.se_action == "session_started":
-            if tst_last_session is not None:
-                days_since_last_session = (x.tst - tst_last_session).total_seconds() / 86400
-            tst_last_session = x.tst
-        if tst_last_event is not None:
-            days_since_last_event = (x.tst - tst_last_event).total_seconds() / 86400
-        tst_last_event = x.tst
+        score = update_score(score, x)
     res = pd.Series(dict(score=score))
     logger.debug(f"final score: {score:.1f}")
     return res
 
 
-def update_score(x0: float, days_since_last_event: float | None, days_since_last_session: float | None, event_type: str, se_action: str):
-    x = update_last_event(x0, days_since_last_event)
-    x = update_last_session(x, days_since_last_session)
-    x = update_signal(x, se_action)
+def update_score(x0: float, f: tuple) -> float:
+    x = update_last_event(x0, f.days_since_last_event)
+    x = update_last_session(x, f.days_since_last_session)
+    x = update_signal(x, f.se_action)
     x = capping(x)
-    logger.debug(f"update_score({x0:.1f}, {days_since_last_event}, {days_since_last_session}, {event_type}, {se_action}) -> {x:.1f}")
+    logger.debug(f"update_score({x0:.1f}, {f}) -> {x:.1f}")
     return x
 
 
